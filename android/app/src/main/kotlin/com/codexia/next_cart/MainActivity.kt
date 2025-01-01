dc3761ef0a33856988e3a83766519a808cd256d4
Vikram <htchonda4@gmail.com>
2025-01-01 20:22:13 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/codexia/next_cart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codexia.next_cart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:next_cart/firebase_options.dart';
+import 'package:next_cart/screens/auth_ui/sign_in_screen.dart';
+import 'package:next_cart/screens/auth_ui/sign_up_screen.dart';
+import 'package:next_cart/screens/auth_ui/splash_screen.dart';
+import 'package:next_cart/screens/user_panel/main_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner:false ,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SignUpScreen(),
+    );
+  }
+}
